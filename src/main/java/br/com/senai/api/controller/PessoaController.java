1c9daedfe1e54bb705664798b182348bbd29bdc5
wellz3n <tomwudkeww@gmail.com>
2021-05-28 09:59:00 -0300

    Third commit

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -2,33 +2,62 @@ package br.com.senai.api.controller;
 
 
 import br.com.senai.domain.model.Pessoa;
+import br.com.senai.domain.repository.PessoaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 
-    @GetMapping("/pessoas")
+//    @PersistenceContext
+//    private EntityManager entityManager;
+
+//    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping()
     public List<Pessoa> listar(){
-        Pessoa pessoa1 = new Pessoa(
-                1L,
-                "Wellynton",
-                "well@gmail.com",
-                "(47)99968-8802"
-        );
-
-        Pessoa pessoa2 = new Pessoa(
-                2L,
-                "Wudke",
-                "wudke@gmail.com",
-                "(47)99167-6849"
-        );
-
-        return Arrays.asList(pessoa1, pessoa2);
+//       return entityManager.createQuery("FROM Pessoa", Pessoa.class).getResultList();
+        return pessoaRepository.findAll();
     }
 
+    @GetMapping("/nome/{pessoaNome}")
+    public List<Pessoa> listarPorNome(@PathVariable String pessoaNome){
+        return pessoaRepository.findByNome(pessoaNome);
+    }
+
+//    @GetMapping("/pessoas/nome")
+//    public List<Pessoa> listarPorNome(){
+//        return pessoaRepository.findByNome("Wudke");
+//    }
+
+    @GetMapping("/nome/containing/{nomeContaining}")
+    public List<Pessoa> listarNomeContaining(@PathVariable String nomeContaining) {
+        return pessoaRepository.findByNomeContaining(nomeContaining);
+    }
+
+    @GetMapping("{pessoaId}")
+    public ResponseEntity<Pessoa> buscar(@PathVariable Long pessoaId){
+//        Optional<Pessoa> pessoa = pessoaRepository.findById(pessoaId);
+//
+//        if(pessoa.isPresent()) {
+//            return ResponseEntity.ok(pessoa.get());
+//        }
+//        return ResponseEntity.notFound().build();
+//
+        return pessoaRepository.findById(pessoaId).map(pessoa -> ResponseEntity.ok(pessoa))
+            .orElse(ResponseEntity.notFound().build());
+    }
 }
