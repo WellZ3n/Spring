9a638b9a993a4a80452c94dbe09478edd6d46e9b
wellz3n <tomwudkeww@gmail.com>
2021-06-01 16:48:52 -0300

    Fifth Commit

--- src/main/java/br/com/senai/domain/repository/PessoaRepository.java
+++ src/main/java/br/com/senai/domain/repository/PessoaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
@@ -12,5 +13,7 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
     List<Pessoa> findByNome(String nome);
 
     List<Pessoa> findByNomeContaining(String nome);
+
+    Optional<Pessoa> findByEmail(String email);
 
 }
