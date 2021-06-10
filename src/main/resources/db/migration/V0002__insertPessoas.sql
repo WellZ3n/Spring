d78c7434779811ecca1a7d256bd9be9aaef07126
Wellynton Wudke <tomwudkeww@gmail.com>
2021-06-10 08:37:13 -0300

    08/06/2021

--- src/main/resources/db/migration/V0002__insertPessoas.sql
+++ src/main/resources/db/migration/V0002__insertPessoas.sql
@@ -1,2 +1,3 @@
-INSERT INTO pessoa VALUES (null, "Well", "well@gmail.com", "937428375"),
+INSERT INTO pessoa VALUES
+(null, "Well" , "well@gmail.com" , "937428375"),
 (null, "Wudke", "wudke@gmail.com", "937428375");
